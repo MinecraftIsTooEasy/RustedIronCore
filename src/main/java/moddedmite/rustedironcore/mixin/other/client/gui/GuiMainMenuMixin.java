7b3b56ddfe0feacfe28d88e431e4a2daf5a8567e
De6ris <2838369930@qq.com>
2025-01-14 21:00:06 +0800

    website change

--- src/main/java/moddedmite/rustedironcore/mixin/other/client/gui/GuiMainMenuMixin.java
+++ src/main/java/moddedmite/rustedironcore/mixin/other/client/gui/GuiMainMenuMixin.java
@@ -3,10 +3,7 @@ package moddedmite.rustedironcore.mixin.other.client.gui;
 import net.minecraft.EnumChatFormatting;
 import net.minecraft.GuiMainMenu;
 import net.minecraft.I18n;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mutable;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,17 +12,22 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GuiMainMenu.class)
 public class GuiMainMenuMixin {
+    @Mutable
+    @Shadow
+    @Final
+    public static String field_96138_a_MITE;
 
-    @Mutable @Shadow @Final public static String field_96138_a_MITE;
+    @Unique
+    private static final String HOMEPAGE = "https://minecraftistooeasy.github.io/";
 
     @ModifyConstant(method = "confirmClicked", constant = @Constant(stringValue = "http://minecraft-is-too-easy.com"))
     private String modifyURI(String constant) {
-        return "https://ysesiq.github.io/modded-mite/";
+        return HOMEPAGE;
     }
 
     @ModifyConstant(method = "mouseClicked", constant = @Constant(stringValue = "http://minecraft-is-too-easy.com"))
     private String modifyURI_1(String constant) {
-        return "https://ysesiq.github.io/modded-mite/";
+        return HOMEPAGE;
     }
 
     @ModifyConstant(method = "drawScreen", constant = @Constant(stringValue = "MITE Resource Pack 1.6.4 needs to be installed!"))
