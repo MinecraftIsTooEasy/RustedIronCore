bb87092ffa0b02a755e9a27b6f52d64497d9f8e5
De6ris <2838369930@qq.com>
2025-05-01 14:52:32 +0800

    fix onInitialBiomesModify

--- src/main/java/moddedmite/rustedironcore/mixin/world/GenLayerBiomeMixin.java
+++ src/main/java/moddedmite/rustedironcore/mixin/world/GenLayerBiomeMixin.java
@@ -11,6 +11,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -21,7 +22,7 @@ public class GenLayerBiomeMixin {
 
     @Inject(method = "<init>(JLnet/minecraft/GenLayer;Lnet/minecraft/WorldType;)V", at = @At("RETURN"))
     private void addBiomes(long par1, GenLayer par3GenLayer, WorldType par4WorldType, CallbackInfo callbackInfo) {
-        List<BiomeGenBase> original = Arrays.stream(this.allowedBiomes).toList();
+        List<BiomeGenBase> original = new ArrayList<>(Arrays.stream(this.allowedBiomes).toList());
         Handlers.BiomeGenerate.onInitialBiomesModify(original);
         this.allowedBiomes = original.toArray(BiomeGenBase[]::new);
     }
